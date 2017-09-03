18dd61a51ef7c84b70d91c24775aab91bf9c5026
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-09-04 00:21:15 +1000

    Create app

---
+++ README.md
@@ -0,0 +1,9 @@
+## droPin
+
+> Leave a message at any location in the world
+
+* Rails API Backend
+* Axios.js for ajax
+* UIkit
+* jQuery
+* _No hosting information yet_
---
+++ js/main.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const pinsUrl = 'https://radiant-plains-19783.herokuapp.com/pins';
+
+function initMap() {
+  const center = {
+    lat: -25.344,
+    lng: 131.036
+  };
+
+  const map = new google.maps.Map(document.getElementById('map'), { zoom: 4, center: center });
+
+  axios.get(pinsUrl).then((response => {
+    if (response.data.length > 0) {
+      response.data.forEach((p) => {
+        const marker = new google.maps.Marker({
+          position: { lat: Number(p.latitude), lng: Number(p.longitude) },
+          map: map
+        });
+        const infowindow = new google.maps.InfoWindow({
+          content: `<h5>${p.message}</h5>`
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+      });
+    }
+  }));
+}
+
+function main() {
+
+  // Initialize form input nodes
+  const $latitude = $('#latitude');
+  const $longitude = $('#longitude');
+  const $message = $('#message');
+  const formInputs = [$latitude, $longitude, $message];
+
+  function validateForm(valid) {
+    formInputs.forEach((n) => {
+      if (n.hasClass('uk-form-danger') && valid) { n.removeClass('uk-form-danger') }
+      if (!n.hasClass('uk-form-danger') && !valid) {
+        n.addClass('uk-form-danger');
+      }
+    });
+  }
+
+  // Function for submitting form data
+  function submitForm() {
+    axios.post(pinsUrl, {
+      latitude: $latitude[0].value,
+      longitude: $longitude[0].value,
+      message: $message[0].value
+    }).then(() => {
+      validateForm(true);
+      initMap();
+      formInputs.forEach((n) => { n.val('') });
+    }).catch((e) => {
+      validateForm(false);
+    })
+  }
+
+  // Handler for form submission
+  $('#submit').click(() => {
+    if (formInputs.every((n) => { return n[0].value !== '' })) {
+      submitForm();
+    }
+    return false;
+  });
+
+}
+
+document.addEventListener('DOMContentLoaded', main);
